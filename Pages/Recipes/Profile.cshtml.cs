e49901f8247599af3b4fec6a4922a95469fc503c
Michał Król <77741@student.pb.edu.pl>
2021-06-10 02:33:19 +0200

    Added List of Authors(need to be repaired)

---
+++ Pages/Recipes/Profile.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NET_Projekt.Models;
+
+namespace NET_Projekt.Pages.Recipes
+{
+   
+    public class ProfileModel : PageModel
+    {
+        private readonly NET_Projekt.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileModel(NET_Projekt.Data.ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IList<Recipe> Recipe { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Recipe = await _context.Recipes
+                .Include(r => r.ApplicationUser).ToListAsync();
+        }
+    }
+}
